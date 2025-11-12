c522a26d6259859fa86538469e413c428c169472
22120419 <nguyenvanty147258@gmail.com>
2025-11-13 02:23:07 +0700

    u

--- components/UserGuide.tsx
+++ components/UserGuide.tsx
@@ -13,7 +13,10 @@ const UserGuide = ({
   if (!isOpen) return null;
 
   return (
-    <div className="w-[90%] md:w-[520px] h-[300px] md:h-[400px] xl:w-[580px] xl:h-[450px] p-5 md:p-8 xl:p-10 flex flex-col justify-center items-center gap-6 md:gap-8 bg-linear-to-br from-[#0a3d15]/40 via-[#0d5720]/35 to-[#0a3d15]/40 backdrop-blur-xs border-[#4ade80] border-2 rounded-3xl drop-shadow-2xl fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50">
+    <div
+      className="w-[90%] md:w-[520px] h-[300px] md:h-[400px] xl:w-[580px] xl:h-[450px] p-5 md:p-8 xl:p-10 
+                flex flex-col justify-center items-center gap-6 md:gap-8 bg-secondary/0 backdrop-blur-xs border-[#4ade80] border-2 rounded-3xl drop-shadow-2xl fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50"
+    >
       <h1 className="text-2xl md:text-2xl xl:text-3xl font-bold text-primary text-center uppercase">
         Hướng dẫn thao tác
       </h1>
